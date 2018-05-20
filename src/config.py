edc04e8b69aba620c9cf47ac2125b537d51e48a6
lvbernal <lvbernal@gmail.com>
2018-05-19 23:48:56 -0500

    receipt printing added

--- src/config.py
+++ src/config.py
@@ -2,22 +2,28 @@
 """Printer configuration file"""
 
 # ----- Local ----- #
-PARKING_LOT_ID = "abc-123-xyz-321"
+PARKING_LOT_ID = "1"
 ID_VENDOR = 0x0fe6
 ID_PRODUCT = 0x811e
 IN_EP = 0x82
 OUT_EP = 0x02
 
 # ----- Application texts ----- #
-CHECKIN = "Ingreso"
-CHECKOUT = "Salida"
-BASE = "Base"
+
+# Should have the same length
+CI_TEXT = "Ingreso: "
+CO_TEXT = "Salida:  "
+
+HELMETS_TEXT = "Cascos"
+BASE_TEXT = "Base"
+HOURS_TEXT = "Hora adicional"
+HOURS_HELMETS_TEXT = "Hora adicional cascos"
+
 CAR = "CARRO"
 PICKUP = "CAMIONETA"
 TRUCK = "CAMION"
 MOTORBIKE = "MOTO"
 BIKE = "BICICLETA"
-HELMETS = "Cascos"
 
 LOADING_CONFIG = "ParqueaderoApp. Configurando..."
 CONFIG_READY = "Impresora lista."
@@ -25,6 +31,6 @@ ERROR_BAD_ID = "Error: El Id del parqueadero no es valido."
 ERROR_BAD_URL = "Error: La url del servidor no es valida o no hay acceso a internet."
 
 # ----- Fixed variables ----- #
-BASE_CONFIG_URL = "server_url"
+BASE_CONFIG_URL = ""
 CONFIG_URL = BASE_CONFIG_URL + PARKING_LOT_ID
 DATE_FORMAT = "%d-%m-%Y %I:%M:%S %p"
--- src/printer_manager.py
+++ src/printer_manager.py
@@ -6,6 +6,7 @@ import json
 from escpos import printer
 from escpos import exceptions
 import config
+import helpers
 
 
 class PrinterManager(object):
@@ -30,6 +31,7 @@ class PrinterManager(object):
 
     def _download_printer_config(self, ptr):
         try:
+            ptr.set(align='left', width=1, height=1)
             ptr.text(config.LOADING_CONFIG + "\n")
 
             with urllib.request.urlopen(config.CONFIG_URL) as url:
@@ -52,17 +54,98 @@ class PrinterManager(object):
 
     def print(self, vehicle):
         """Print receipt for vehicle"""
-        return vehicle
+        done = vehicle.get("done", False)
+
+        ptr = self.get_printer()
+        self._print_header(ptr)
+        ptr.text("\n\n")
+        self._print_plate(ptr, vehicle)
+        ptr.text("\n")
+        self._print_common_content(ptr, vehicle)
+        ptr.text("\n")
+
+        if done:
+            self._print_checkout_info(ptr, vehicle)
+            ptr.text("\n\n")
+            self._print_fee_details(ptr, vehicle)
+        else:
+            ptr.text("\n")
+            self._print_checkin_info(ptr, vehicle)
+            ptr.text("\n")
+            self._print_plate_qr(ptr, vehicle)
+            ptr.text("\n\n")
+            self._print_contract(ptr)
+
+        ptr.cut()
+        ptr.close()
+        return True
 
     def _print_header(self, ptr):
-        ptr.set(align='center')
+        ptr.set(align='center', width=1, height=1)
         ptr.text(self.header)
+
+    def _print_plate(self, ptr, vehicle):
+        plate = vehicle.get("plate", "")
+        vehicle_type = vehicle.get("vehicle_type", "")
+        v_str = helpers.get_vehicle_name(vehicle_type)
+        ptr.set(align='center', width=2, height=2)
+        ptr.text(v_str + " " + plate)
+
+    def _print_common_content(self, ptr, vehicle):
+        helmets = vehicle.get("helmets", 0)
+        checkin = vehicle.get("check_in", "")
+        ci_str = helpers.format_date(checkin)
+
+        ptr.set(align='center', width=1, height=1)
+        if helmets > 0:
+            ptr.text(config.HELMETS_TEXT + ": " + str(helmets) + "\n")
         ptr.text("\n")
+        ptr.text(config.CI_TEXT + ci_str)
+
+    def _print_checkout_info(self, ptr, vehicle):
+        checkout = vehicle.get("check_out", "")
+        co_str = helpers.format_date(checkout)
+        ptr.text(config.CO_TEXT + co_str)
+
+    def _print_fee_details(self, ptr, vehicle):
+        fee_detail = vehicle.get("fee_detail", None)
+        helmets = vehicle.get("helmets", 0)
+        fee = vehicle.get("fee", 0)
+
+        ptr.set(align='left')
+        ptr.text(config.BASE_TEXT + ": \t\t$" + str(fee_detail["baseFee"]) + "\n")
+        ptr.text(config.HOURS_TEXT + "(" + str(int(fee_detail["additionalHours"])) + "): \t\t$" +
+                 str(fee_detail["additionalFee"]) + "\n")
+
+        if helmets > 0:
+            ptr.text(config.HELMETS_TEXT + ": \t\t$" + str(fee_detail["helmetsTotal"]) + "\n")
+
+        ptr.set(align='center', width=2, height=2)
+        ptr.text("\n")
+        ptr.text("$" + str(fee))
+
+    def _print_checkin_info(self, ptr, vehicle):
+        fee_detail = vehicle.get("fee_detail", None)
+        helmets = vehicle.get("helmets", 0)
+
+        ptr.set(align='left')
+        ptr.text(config.BASE_TEXT + ": \t\t$" + str(fee_detail["baseValue"]) + "\n")
+        ptr.text(config.HOURS_TEXT + ": \t\t$" + str(fee_detail["feeValue"]) + "\n")
+
+        if helmets > 0:
+            ptr.text(config.HELMETS_TEXT + ": \t\t$" + str(fee_detail["helmetsBase"]) + "\n")
+
+            helmets_fee = fee_detail["helmetsFee"]
+            if helmets_fee > 0:
+                ptr.text(config.HOURS_HELMETS_TEXT + ": \t\t$" + str(helmets_fee) + "\n")
+
+    def _print_plate_qr(self, ptr, vehicle):
+        plate = vehicle.get("plate", "")
+        ptr.qr(plate, size=8)
 
     def _print_contract(self, ptr):
         ptr.set(align='left')
         ptr.text(self.contract)
-        ptr.text("\n")
 
     def get_printer(self):
         """Create printer instance from configuration"""
